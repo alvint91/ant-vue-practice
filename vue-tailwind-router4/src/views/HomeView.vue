e2de9a6e907150dff53018e286fa61b7c83368b5
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-09-09 16:47:35 +0000

    Desktop UI updates wip

--- vue-tailwind-router4/src/views/HomeView.vue
+++ vue-tailwind-router4/src/views/HomeView.vue
@@ -1,20 +1,37 @@
 
 <template>
-  <div v-if="viewportWidth <= 800" class="h-[312px]">
-    <img
-      src="https://dm3381rcqf07k.cloudfront.net/multisite.insomniac.com/wp-content/uploads/sites/56/2022/05/25081917/edcw_2022_mk_an_fs_mh_750x600_r01.jpg"
-      alt="" />
+  <!-- <div v-if="viewportWidth <= 800" class="h-[312px]"> -->
+  <!-- <div v-if="viewportWidth < 800">
+    <h1>hi</h1>
   </div>
   <div v-else>
+    <h1>{{test}}</h1>
+  </div> -->
+  <div>
+    <h1>test</h1>
+  </div>
+    <!-- <div class="h-[312px]">
+  <img
+      src="https://dm3381rcqf07k.cloudfront.net/multisite.insomniac.com/wp-content/uploads/sites/56/2022/05/25081917/edcw_2022_mk_an_fs_mh_750x600_r01.jpg"
+      alt="" />
+  </div> -->
+  <!-- <div v-else>
     <img
       src="https://dm3381rcqf07k.cloudfront.net/multisite.insomniac.com/wp-content/uploads/sites/56/2022/05/25081913/edcw_2022_mk_an_fs_dh_1600x800_r01.jpg"
       alt=""
       class="w-full object-center object-cover">
-  </div>
+  </div> -->
 </template>
 
 <script>
 let viewportWidth = window.innerWidth;
+console.log(viewportWidth)
+export default {
+  data() {
+    test: viewportWidth
+  }
+}
+
 </script>
 
 <style></style>
