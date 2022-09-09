e603dc100b118529103fe5c4e69b73de29ddc280
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-09-09 08:49:01 +0000

    Desktop UI updates wip

--- vue-tailwind-router4/src/components/NavComponent.vue
+++ vue-tailwind-router4/src/components/NavComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-center px-[25px] py-5 mx-auto bg-black h-[80px]">
+  <div class="flex justify-between items-center px-[25px] py-5 mx-auto bg-black h-[80px] fixed top-0 w-full sm:p-5 sm:h-[140px]">
     <div>
       <RouterLink to="/">
         <h3 class="text-2xl font-medium text-white text-500">LOGO</h3>
