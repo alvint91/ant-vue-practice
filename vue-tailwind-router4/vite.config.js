9b277d7b7cbaf4e75c377e4f8917e5307376c6ad
Alvin Truong <alvintruong91@gmail.com>
2022-10-03 20:54:12 -0700

    chore

--- vue-tailwind-router4/vite.config.js
+++ vue-tailwind-router4/vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: `ant-vue-practice/vue-tailwind-router4/dist/`
 });
